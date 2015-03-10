1efaafb201f1fc23e1efe20c54188f837597f087
Maxx Wyndham <maxx@marketingradar.com>
2015-03-10 07:02:37 +0000

    Particles no longer function

--- thatGameEngine/Graphics/Particle.cs
+++ thatGameEngine/Graphics/Particle.cs
@@ -15,33 +15,42 @@ namespace thatGameEngine
         Single damping = 0.995f;
         Single inverseMass = 1.0f;
 
-        public Model model;
+        Vector3 forceAccum;
+
+        static Model model;
+        static Texture texture;
 
         Vector3 gravity = new Vector3(0, -9.2f, 0);
 
         public Particle()
         {
-            var texture = new Texture();
-            texture.CreateFromBitmap(new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("thatGameEngine.Data.entity.png")), "entity");
+            if (texture == null)
+            {
+                texture = new Texture();
+                texture.CreateFromBitmap(new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("thatGameEngine.Data.entity.png")), "entity");
+            }
 
-            var sprite = new ModelMeshPart();
-            sprite.AddVertex(new Vector3(-0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(0, 1));
-            sprite.AddVertex(new Vector3(-0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(0, 0));
-            sprite.AddVertex(new Vector3(0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(1, 0));
-            sprite.AddVertex(new Vector3(0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(1, 1));
+            if (model == null)
+            {
+                var sprite = new ModelMeshPart();
+                sprite.AddVertex(new Vector3(-0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(0, 1));
+                sprite.AddVertex(new Vector3(-0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(0, 0));
+                sprite.AddVertex(new Vector3(0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(1, 0));
+                sprite.AddVertex(new Vector3(0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(1, 1));
 
-            sprite.AddVertex(new Vector3(0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(0, 1));
-            sprite.AddVertex(new Vector3(0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(0, 0));
-            sprite.AddVertex(new Vector3(-0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(1, 0));
-            sprite.AddVertex(new Vector3(-0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(1, 1));
-            sprite.IndexBuffer.Initialise();
-            sprite.VertexBuffer.Initialise();
-            sprite.Material = new Material { Name = "Entity.Asset", Texture = texture };
-            sprite.PrimitiveType = PrimitiveType.Quads;
-            var spritemesh = new ModelMesh();
-            spritemesh.AddModelMeshPart(sprite);
-            model = new Model();
-            model.AddMesh(spritemesh);
+                sprite.AddVertex(new Vector3(0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(0, 1));
+                sprite.AddVertex(new Vector3(0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(0, 0));
+                sprite.AddVertex(new Vector3(-0.05f, 0.0f, 0.05f), Vector3.UnitY, new Vector2(1, 0));
+                sprite.AddVertex(new Vector3(-0.05f, 0.0f, -0.05f), Vector3.UnitY, new Vector2(1, 1));
+                sprite.IndexBuffer.Initialise();
+                sprite.VertexBuffer.Initialise();
+                sprite.Material = new Material { Name = "Entity.Asset", Texture = texture };
+                sprite.PrimitiveType = PrimitiveType.Quads;
+                var spritemesh = new ModelMesh();
+                spritemesh.AddModelMeshPart(sprite);
+                model = new Model();
+                model.AddMesh(spritemesh);
+            }
 
             Position = new Vector3((Single)(SceneManager.Current.Rando.NextDouble() * 8 - 4), 15, (Single)(SceneManager.Current.Rando.NextDouble() * 8 - 4));
         }
@@ -56,15 +65,29 @@ namespace thatGameEngine
             inverseMass = mass;
         }
 
+        public void addForce(Vector3 force)
+        {
+            forceAccum += force;
+        }
+
+        public void ClearAccumulator()
+        {
+            forceAccum = Vector3.Zero;
+        }
+
         public override void Update(Single dt)
         {
             base.Update(dt);
 
-            Position += Velocity * dt;
+            Vector3.Add(Position, Velocity * dt);
 
             Vector3 rA = Acceleration;
-            rA += gravity * inverseMass;
-            Velocity += rA * dt;
+            Vector3.Add(rA, forceAccum * inverseMass);
+            Vector3.Add(Velocity, rA * dt);
+
+            Velocity *= (Single)Math.Pow(damping, dt);
+
+            ClearAccumulator();
 
             if (Position.Y < 0) { Position.Y = 0.0001f; }
         }
